3b2ce51f4d7e65208e8e20bea0d8d7baf43234e3
shreyas agarwal <you@example.com>
2026-01-04 01:03:08 -0600

    background and stuff

--- scripts/slime_1.gd
+++ scripts/slime_1.gd
@@ -1,5 +1,10 @@
 extends Area2D
-
+var health = 2
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("bullets"):
+		healthTick()
+		
+func healthTick():
+	health -= 1
+	if(health == 0):
 		queue_free()
