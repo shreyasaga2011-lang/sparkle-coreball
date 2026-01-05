375a6d593088dd97d00de89b5a23e9cf2e49bdc5
shreyas agarwal <you@example.com>
2026-01-04 18:20:24 -0600

    asd

--- scripts/slime_1.gd
+++ scripts/slime_1.gd
@@ -1,6 +1,6 @@
 extends Area2D
 var health = 2
-@onready var kill_tracker: Label = $Camera2D/KillTracker
+@onready var kill_tracker: Label = get_tree().get_first_node_in_group("kill_tracker")
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("bullets"):
@@ -9,5 +9,5 @@ func _on_area_entered(area: Area2D) -> void:
 func healthTick():
 	health -= 1
 	if(health == 0):
+		kill_tracker.upper()
 		queue_free()
-		KillTracker.upper()
